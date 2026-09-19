3023c9adc060fc9a68c8d3d555fa36e2c3d7440b
mastermoh54-lang <mastermoh54@gmail.com>
2026-09-19 23:06:21 +0300

    Update page.tsx

--- app/(app)/movies/[id]/page.tsx
+++ app/(app)/movies/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { MovieDetailClient } from "./MovieDetailClient";
 
 export async function generateStaticParams() {
-  return [];
+  return [{ id: "placeholder" }];
 }
 
 export default async function MovieDetailPage({
