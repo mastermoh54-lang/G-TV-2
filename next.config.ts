a1da9241d9986347bbc95ac2c59a4e9da05060dc
mastermoh54-lang <mastermoh54@gmail.com>
2026-09-20 08:53:03 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,23 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  
+  // 1. Autoriser Cloudflare (CORS)
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+        ]
+      }
+    ]
+  },
+
+  // 2. Tes redirections vers Railway et Vercel
   async rewrites() {
     return [
       // --- VERCEL : Standalone Live TV & Services Xtream ---
